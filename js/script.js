0109e71768983b134a1a8da2f81df351f79f2046
Shambu Nath Singh <107608721+shambunathsingh@users.noreply.github.com>
2022-08-23 11:39:28 +0530

    commit 1
    commit 1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var menuList = document.getElementById("menuList");
+
+menuList.style.maxHeight = "0px";
+
+function togglemenu() {
+  if (menuList.style.maxHeight == "0px") {
+    menuList.style.maxHeight = "150px";
+  } else {
+    menuList.style.maxHeight = "0px";
+  }
+}
